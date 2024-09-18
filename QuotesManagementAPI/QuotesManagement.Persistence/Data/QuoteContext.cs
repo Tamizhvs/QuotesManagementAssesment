7a92986924fd6c5844b09efdfe9fcedb6a82b8e3
Tamizhvs <tamizh@vsonlineservices.com>
2024-09-18 11:13:45 +0530

    Quotes Management Api project added

---
+++ QuotesManagementAPI/QuotesManagement.Persistence/Data/QuoteContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuotesManagement.Persistence.Data
+{
+    public class QuoteContext
+    {
+    }
+}
